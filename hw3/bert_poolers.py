f3e46853c66e186983ebe1c7f326c305739c3356
하현정 <gkguswjd2103@gmail.com>
2021-05-09 17:34:04 +0900

    bert_pooler

--- hw3/bert_poolers.py
+++ hw3/bert_poolers.py
@@ -10,19 +10,40 @@ from transformers.models.bert.modeling_bert import (
 class MeanMaxTokensBertPooler(nn.Module):
     def __init__(self, config):
         super().__init__()
-        raise NotImplementedError
+        self.linearTransform = nn.Linear(2*config.hidden_size, config.hidden_size)
+        self.tanh = nn.Tanh()
 
     def forward(self, hidden_states, *args, **kwargs):
-        raise NotImplementedError
+        tmp = torch.sum(hidden_states,dim=1)
+        szs = torch.norm(hidden_states, dim=2)
+        idx = torch.argmax(szs, dim=1)
+        tmp2 =torch.Tensor()
+        tmp2 = tmp2.to('cuda')
+        
+        for i,val in enumerate(idx):
+          t = hidden_states[i,val,:]
+          tmp2 = torch.cat((tmp2,t.view(-1, len(t))))
+        tmp = torch.cat((tmp, tmp2),dim=1)
+        
+        return self.tanh(self.linearTransform(tmp))
 
 
 class MyBertPooler(nn.Module):
     def __init__(self, config):
         super().__init__()
-        raise NotImplementedError
+
+    def softmax_windows(self, x):
+        x_strided = x.view(x.shape[0], -1, 2)
+        x_softmax_windows = nn.functional.softmax(x_strided, dim=-1)
+        x_strided_softmax = x_softmax_windows.view(x_softmax_windows.shape[0], -1)
+
+        return x_strided_softmax
 
     def forward(self, hidden_states, *args, **kwargs):
-        raise NotImplementedError
+        x = hidden_states[:, 0]
+        pooled_output = self.softmax_windows(x)* x
+        pooled_output = nn.functional.relu(pooled_output)
+        return pooled_output
 
 
 class MyBertConfig(BertConfig):
@@ -82,7 +103,7 @@ class MyBertForSequenceClassification(BertForSequenceClassification):
             output_hidden_states=None,
             return_dict=None,
     ):
-        if self.bert.pooling_layer_type in ["CLS", "MEAN_MAX"]:
+        if self.bert.pooling_layer_type in ["CLS", "MEAN_MAX", "MINE"]:
             return super().forward(
                 input_ids, attention_mask, token_type_ids, position_ids, head_mask,
                 inputs_embeds, labels, output_attentions, output_hidden_states, return_dict
